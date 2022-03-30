a194b40735813c64c0d82e1c353b2bad9e544e36
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-03-30 16:43:49 +0200

    Update easy3dlauncher.sh

--- files/builds/development-branch/easy3dlauncher.sh
+++ files/builds/development-branch/easy3dlauncher.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2022                                                                               #
-# Time/Date:    15:30/30.03.2022                                                                   #
-# Version:      0.0.2                                                                              #
+# Time/Date:    16:45/30.03.2022                                                                   #
+# Version:      0.0.3                                                                              #
 ####################################################################################################
 
 # Path: /$HOME/.config/Easy3DLauncher/bin/shortcuts-dialog.sh
@@ -26,15 +26,14 @@ exec 3<> $Easy3DLauncher
 yad --notification --command='./shortcuts-dialog.sh' --listen <&3 &
 # Menü wird definiert:
 >&3 echo "menu:\
-Shortcuts ...!./shortcuts-dialog.sh|\
-Workspace ...!./workspace-dialog.sh|\
-Documentation ...!./help-dialog.sh|\
-Settings ...!./config-dialog.sh|\
-About ...!./about-dialog.sh|\
-------------------------------!continue|\
-Exit ...!pkill yad!system-shutdown-symbolic \ \
+Shortcuts ...!./shortcuts-dialog.sh!/$HOME/.local/share/icons/Easy3DLauncher/favorite.svg|\
+Workspace ...!./workspace-dialog.sh!/$HOME/.local/share/icons/Easy3DLauncher/window.svg|\
+Documentation ...!./help-dialog.sh!/$HOME/.local/share/icons/Easy3DLauncher/help.svg|\
+Settings ...!./config-dialog.sh!/$HOME/.local/share/icons/Easy3DLauncher/settings.svg|\
+About ...!./about-dialog.sh!/$HOME/.local/share/icons/Easy3DLauncher/about.svg|\
+Exit ...!pkill yad!/$HOME/.local/share/icons/Easy3DLauncher/exit.svg \
 "
 # Tooltip wird definiert:
 >&3 echo "TOOLTIP:Easy3DLauncher - This tool allows you to execute some commands with using the mouse."
 # Icon wird definiert:
->&3 echo "icon:/usr/share/icons/easy3dlauncher/easy3dlauncher.png"
+>&3 echo "icon:/$HOME/.local/share/icons/Easy3DLauncher/launcher.svg"
