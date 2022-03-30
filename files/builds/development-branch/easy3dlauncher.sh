2af0bb9b99607302e948b2ea2b4d9101269b33fd
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-03-30 15:13:06 +0200

    Update easy3dlauncher.sh

--- files/builds/development-branch/easy3dlauncher.sh
+++ files/builds/development-branch/easy3dlauncher.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-FIFO="$(mktemp)"
-rm $FIFO
-mkfifo $FIFO
+####################################################################################################
+# Name:         Easy3DLauncher for Linux - Launcher (Linux)                                        #
+# Description:  With this program you can execute some commands with the mouse on Linux.           #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2022                                                                               #
+# Time/Date:    15:15/30.03.2022                                                                   #
+# Version:      0.0.1                                                                              #
+####################################################################################################
+
+# Path: /$HOME/.config/Easy3DLauncher/bin/shortcuts-dialog.sh
+
+###############################################################################################################################################################
+# ALL LOG-FUNCTIONS ARE ARRANGED HERE:                                                                                                                        #
+###############################################################################################################################################################
+
+Easy3DLauncher="$(mktemp)"
+rm $Easy3DLauncher
+mkfifo $Easy3DLauncher
 # Kanal wird etabliert:
-exec 3<> $FIFO
+exec 3<> $Easy3DLauncher
 # Yad wird gestartet:
-yad --notification --command='cmd -c pacui u' --listen <&3 &
+yad --notification --command='./shortcuts-dialog.sh' --listen <&3 &
 # Menü wird definiert:
 >&3 echo "menu:\
 Shortcuts ...!./shortcuts-dialog.sh|\
@@ -14,9 +31,10 @@ Workspace ...!./workspace-dialog.sh|\
 Documentation ...!./help-dialog.sh|\
 Settings ...!./config-dialog.sh|\
 About ...!./about-dialog.sh|\
-Close ...!pkill yad \
+------------------------------!continue|\
+Exit ...!pkill yad \
 "
 # Tooltip wird definiert:
 >&3 echo "TOOLTIP:Easy3DLauncher - This tool allows you to execute some commands with using the mouse."
 # Icon wird definiert:
->&3 echo "icon:/usr/share/icons/Adwaita/16x16/emblems/emblem-favorite-symbolic.symbolic.png"
+>&3 echo "icon:/usr/share/icons/easy3dlauncher/easy3dlauncher.png"
