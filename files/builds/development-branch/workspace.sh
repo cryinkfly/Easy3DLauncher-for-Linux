6ec8cd046d7db6e16b7b60a7db0ce63cde0787d4
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-04-16 10:37:27 +0200

    Update workspace.sh

--- files/builds/development-branch/workspace.sh
+++ files/builds/development-branch/workspace.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2022                                                                               #
-# Time/Date:    10:00/06.04.2022                                                                   #
-# Version:      0.0.2                                                                              #
+# Time/Date:    10:30/16.04.2022                                                                   #
+# Version:      0.0.3                                                                              #
 ####################################################################################################
 
 # Path: /$HOME/.config/Easy3DLauncher/bin/workspace.sh
@@ -18,9 +18,10 @@
 ###############################################################################################################################################################
 
 
-answer=$(TK_THEME="easy3dlauncher" \
+answer=$(GTK_THEME="easy3dlauncher" \
 yad \
---checklist \
+--width=350 \
+--radiolist \
 --list \
 --column=Select \
 --column=Software \
@@ -29,16 +30,22 @@ false "FreeCAD" \
 --title="Easy3DLauncher - Workspace" \
 --undecorated \
 --center \
---button="Cancel:1"  \
---button="Ok:0" )
+--button="Add:1"  \
+--button="Edit:2"  \
+--button="Delete:3"  \
+--button="Save:0" )
 
 ret=$?
 
 # Responses to above button presses are below:
 
+
 if [[ $ret -eq 1 ]]; then
-    echo "Settings"
-    . settings.sh
+    echo "Add"
+elif [[ $ret -eq 2 ]]; then
+    echo "Edit"
+elif [[ $ret -eq 3 ]]; then
+    echo "Remove"
 elif [[ $answer = "TRUE|Autodesk Fusion 360|" ]]; then
     sed -i '17s/.*/Workspace=1/' $HOME/.config/Easy3DLauncher/profiles/profile.config
 elif [[ $answer = "TRUE|FreeCAD|" ]]; then
