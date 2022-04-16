e33d50132d7cbefb0ff15462a9cb729d29d7e221
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-04-16 11:01:30 +0200

    Update workspace.sh

--- files/builds/development-branch/workspace.sh
+++ files/builds/development-branch/workspace.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2022                                                                               #
-# Time/Date:    10:30/16.04.2022                                                                   #
-# Version:      0.0.3                                                                              #
+# Time/Date:    11:00/16.04.2022                                                                   #
+# Version:      0.0.4                                                                              #
 ####################################################################################################
 
 # Path: /$HOME/.config/Easy3DLauncher/bin/workspace.sh
@@ -16,7 +16,6 @@
 ###############################################################################################################################################################
 # ALL LOG-FUNCTIONS ARE ARRANGED HERE:                                                                                                                        #
 ###############################################################################################################################################################
-
 
 answer=$(GTK_THEME="easy3dlauncher" \
 yad \
@@ -30,21 +29,23 @@ false "FreeCAD" \
 --title="Easy3DLauncher - Workspace" \
 --undecorated \
 --center \
---button="Add:1"  \
---button="Edit:2"  \
---button="Delete:3"  \
+--button="Reset:1"  \
+--button="Add:2"  \
+--button="Edit:3"  \
+--button="Delete:4"  \
 --button="Save:0" )
 
 ret=$?
 
 # Responses to above button presses are below:
-
-
 if [[ $ret -eq 1 ]]; then
-    echo "Add"
+    echo "Reset"
+    sed -i '17s/.*/Workspace=0/' $HOME/.config/Easy3DLauncher/profiles/profile.config
 elif [[ $ret -eq 2 ]]; then
-    echo "Edit"
+    echo "Add"
 elif [[ $ret -eq 3 ]]; then
+    echo "Edit"
+elif [[ $ret -eq 4 ]]; then
     echo "Remove"
 elif [[ $answer = "TRUE|Autodesk Fusion 360|" ]]; then
     sed -i '17s/.*/Workspace=1/' $HOME/.config/Easy3DLauncher/profiles/profile.config
